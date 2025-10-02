c6f08b32fb3b89cc2d0ad65c1587d4879043ea33
safeone1 <safeonekan@gmail.com>
2025-10-02 13:20:31 +0100

    client side started

--- src/main/java/com/example/demo/handlers/PageEventHandler.java
+++ src/main/java/com/example/demo/handlers/PageEventHandler.java
@@ -1,12 +1,21 @@
 package com.example.demo.handlers;
 
+import java.sql.Time;
+import java.time.Duration;
 import java.util.Date;
 import java.util.Random;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.Grouped;
 import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.TimeWindows;
+import org.apache.kafka.streams.kstream.internals.TimeWindow;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
@@ -17,7 +26,7 @@ public class PageEventHandler {
     @Bean
     public Consumer<PageEvent> pageEventConsumer() {
         return (input) -> {
-            System.out.println("Page event received: " + input);
+            System.out.println("Page event received: " + input.toString());
             System.out.println("Page name: " + input.name());
         };
     }
@@ -29,10 +38,15 @@ public class PageEventHandler {
     }
     
     @Bean
-    public Function<KStream<String, PageEvent>, KStream<String, String>> kStreamFunction() {
+    public Function<KStream<String, PageEvent>, KStream<String, Long>> kStreamFunction() {
         return (input) -> input
-                .filter((key, value) -> value.duration() > 1000)
-                .mapValues(value -> "Page event filtered: " + value);
-                
+                .filter((key, value) -> value.duration() > 100)
+                .map((key, value) -> new KeyValue<>(value.name(), value.duration()))
+                .groupByKey(Grouped.with(Serdes.String(), Serdes.Long()))
+                .windowedBy(TimeWindows.of(Duration.ofSeconds(5)))
+                .count(Materialized.as("counts-store"))
+                .toStream()
+                .map((k, v) -> new KeyValue<>(k.key(), v));
+
     }
 }
